58566058f3bb4d9b68e93852ed750d2174167d7c
sameekshatarate <165119806+sameekshatarate@users.noreply.github.com>
2024-09-26 14:09:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-project-terraform-1"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table1"
   }
 }
